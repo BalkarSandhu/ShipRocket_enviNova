e34447ee72810979475f6e57bf9ec2e4aa1e93f0
Balkar <balkarsandhu321@gmail.com>
2022-04-15 22:21:28 +0530

    asdf

--- Desktop/jobportal/src/App.js
+++ Desktop/jobportal/src/App.js
@@ -31,89 +31,54 @@ function App() {
     user: {},
 })
 const navigate = useNavigate()
-useEffect(() => {
-    const loggedInUser = localStorage.getItem("keewe.lmsStorage");
-    if (loggedInUser) {
-        const foundUser = JSON.parse(loggedInUser);
-        axios({
-            // url: "https://lmsapi.keewesolutions.com/check",
-            url: 'http://localhost:8081/check',
-            method: "POST",
-            data: {
-                token: foundUser.user.idToken
-            }
-        }).then(res=>{
-            if(res.data.callStatusCode === 200){
-                let newAuthState = {...authState}
-                newAuthState.authenticated = true
-                let newPayload = res.data.response
-                let newUser = {
-                    id: newPayload['cognito:username'],
-                    email: newPayload.email,
-                    idToken: foundUser.user.idToken,
-                    refreshToken: foundUser.user.refreshToken.token
-                  }
-                  newAuthState.user = newUser
-                setAuthState(newAuthState)
-            }else{
-                navigate('/login')
-            }
-        })
+// useEffect(() => {
+//     const loggedInUser = localStorage.getItem("keewe.lmsStorage");
+//     if (loggedInUser) {
+//         const foundUser = JSON.parse(loggedInUser);
+//         axios({
+//             // url: "https://lmsapi.keewesolutions.com/check",
+//             url: 'http://localhost:8081/check',
+//             method: "POST",
+//             data: {
+//                 token: foundUser.user.idToken
+//             }
+//         }).then(res=>{
+//             if(res.data.callStatusCode === 200){
+//                 let newAuthState = {...authState}
+//                 newAuthState.authenticated = true
+//                 let newPayload = res.data.response
+//                 let newUser = {
+//                     id: newPayload['cognito:username'],
+//                     email: newPayload.email,
+//                     idToken: foundUser.user.idToken,
+//                     refreshToken: foundUser.user.refreshToken.token
+//                   }
+//                   newAuthState.user = newUser
+//                 setAuthState(newAuthState)
+//             }else{
+//                 navigate('/login')
+//             }
+//         })
         
 
-    }
-}, []);
+//     }
+// }, []);
   return (
    
     <queryContext.Provider value={{
       value: query,
       setValue: setQuery
     }}>
-      <UserContext.Provider value={{ ...authState, setNewUser: setAuthState }}>
-                            {authState.authenticated ?
-                            <Routes>
-                               <Route path="/" element={<Findjobs />} />
-                               <Route path="/findjobs" element={<Findjobs />} />
-                               <Route path="/searchcompanies" element={<Browser />} />
-                               <Route path="/companiesresult" element={<Searchcompaniesresult />} />
-                               <Route path="/jobdescription" element={<Jobdescription />} />
-                               <Route path="/companyprofile" element={<Companyprofile />} />
-                               </Routes>
-                                : (
-                                    <>
-                                        <Routes>
-                                            <Route path="/" element={<RegisterCard id={1} />} />
-                                            <Route path="register" element={<RegisterCard id={0} />} />
-                                            <Route path="login" element={<RegisterCard id={1} />} />
-                                            <Route path="verify" element={<Verification />} />
-                                            <Route path="forgotpassword" element={<ForgotPassword />} />
-                                            <Route path="changepassword" element={<NewPassword />} />
-                                            {/* <Route path="/detail" element={<Agriculture dataArr={dataArr} />}/> */}
-                                        </Routes>
-                                    </>)
+      <Routes>
+          <Route path="/" element={<Findjobs />} />
+          <Route path="/findjobs" element={<Findjobs />} />
+          <Route path="/searchcompanies" element={<Browser />} />
+          <Route path="/companiesresult" element={<Searchcompaniesresult />} />
+          <Route path="/jobdescription" element={<Jobdescription />} />
+          <Route path="/companyprofile" element={<Companyprofile />} />
+          </Routes>
+      </queryContext.Provider>
 
-                            }
-                        </UserContext.Provider>
-        {/* <Router>
-          <Routes>
-            <Route path='/' element={<RegisterCard id={1}/>}/>
-            <Route path='/login' element={<RegisterCard id={1}/>}/>
-            <Route path='/register' element={<RegisterCard id={0}/>}/> */}
-            {/* <Route path="/" element={<Findjobs />} />
-            <Route path="/findjobs" element={<Findjobs />} />
-            <Route path="/searchcompanies" element={<Browser />} />
-            <Route path="/companiesresult" element={<Searchcompaniesresult />} />
-            <Route path="/jobdescription" element={<Jobdescription />} />
-            <Route path="/companyprofile" element={<Companyprofile />} /> */}
-            {/* <Route path='/postajob' element = {<Combine/>}/>
-            <Route path='/step2' element = {<JobDescription/>}/>
-      <Route path= '/jobinformation' element = {<JobInformation/>}/> */}
-      {/* <Route path= '/jobdescription' element = {<JobDescription/>}/> */}
-s
-          {/* </Routes>
-        </Router> */}
-     
-    </queryContext.Provider>
   );
 }
 
